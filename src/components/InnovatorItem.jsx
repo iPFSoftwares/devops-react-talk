fec7db47fce567971d3e966e4d501573106a9f20
Walter Kimaro <wakyj07@gmail.com>
2020-11-21 14:23:36 +0300

    Add correct innovator image alt text

--- src/components/InnovatorItem.jsx
+++ src/components/InnovatorItem.jsx
@@ -8,7 +8,7 @@ const InnovatorItem = ({stakeholder}) => {
         <div className="bg-gray-800 mt-4 flex flex-col rounded-md overflow-hidden border-2 border-gray-700">
             <div className="mb-4 bg-white pt-16 bg-cover bg-center" style={{backgroundImage: `url(${mapImage})`}}>
                 <div className="bg-white ml-4 mt-4 -mb-8 w-16 h-16 rounded-full border-8 border-gray-800 overflow-hidden mr-4 flex-shrink-0 relative">
-                    <img className="absolute w-full h-full inset-0 object-cover" src={logo} alt="Logo" />
+                    <img className="absolute w-full h-full inset-0 object-cover" src={logo} alt="Innovator Logo" />
                 </div>
             </div>
 
