66e31f84eb17c4d73c789d5e3e61be6627c1b6bc
Walter Kimaro <wakyj07@gmail.com>
2020-11-21 14:12:50 +0300

    Adapt new design

--- src/components/InnovatorItem.jsx
+++ src/components/InnovatorItem.jsx
@@ -2,18 +2,21 @@ import React from 'react';
 
 const InnovatorItem = ({stakeholder}) => {
     const {logo, name, locationName, location} = stakeholder;
-
+    const mapImage = `https://www.mapquestapi.com/staticmap/v4/getplacemap?key=HkQT4BGfQ9clGrwhLknjPxeF08UZ3k9K&location=${location.coordinates.reverse().join(',')}&size=400,200&type=map&zoom=13&imagetype=jpg&scalebar=false&`;
+    
     return (
-        <div class="bg-gray-800 mt-4 flex flex-col rounded-md overflow-hidden border-2 border-gray-700">
-            <div class="bg-white ml-4 mt-4 w-12 h-12 rounded overflow-hidden mr-4 flex-shrink-0 relative">
-                <img class="absolute w-full h-full inset-0 object-cover" src={logo} alt="Logo" />
+        <div className="bg-gray-800 mt-4 flex flex-col rounded-md overflow-hidden border-2 border-gray-700">
+            <div className="mb-4 bg-white pt-16 bg-cover bg-center" style={{backgroundImage: `url(${mapImage})`}}>
+                <div className="bg-white ml-4 mt-4 -mb-8 w-16 h-16 rounded-full border-8 border-gray-800 overflow-hidden mr-4 flex-shrink-0 relative">
+                    <img className="absolute w-full h-full inset-0 object-cover" src={logo} alt="Logo" />
+                </div>
             </div>
 
-            <div class="p-4">
-                <h3 class="text-sm font-semibold">
+            <div className="p-4">
+                <h3 className="text-sm font-semibold">
                     { name }
                 </h3>
-                <p class="mt-1 capitalize text-sm leading-snug opacity-75">
+                <p className="mt-1 capitalize text-sm leading-snug opacity-75">
                     { locationName }
                 </p>
             </div>
